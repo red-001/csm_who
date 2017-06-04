ed051fa83e9efcd9498aed0d468aa6c77242d9d7
red-001 <red-001@outlook.ie>
2017-06-04 22:26:40 +0100

    Add buttons to ban/kick players + minor bugfix
    Add buttons for baning and kicking players and fix there being an extra empty entry in the player list.

--- init.lua
+++ init.lua
@@ -26,44 +26,85 @@ SOFTWARE.
 
 ]]
 
+
 local localplayer
+local player_names
+local selected_player = ""
 
 minetest.register_on_connect(function()
 	localplayer = minetest.localplayer
+	minetest.display_chat_message("[CSM] loaded Who")
 end)
 
-local function show_players()
+local function show_main_dialog()
 
-	local player_names = minetest.get_player_names()
+	player_names = minetest.get_player_names()
 	table.sort(player_names)
-	local formspec = "size[3,6]" ..
+
+	local size = "size[3,6]"
+	if selected_player ~= "" then
+		size = "size[4.5,6]"
+	end
+	local formspec = size ..
 		"bgcolor[#080808BB;true]" ..
 		"background[5,5;1,1;gui_formbg.png;true]" ..
 		"label[0.27,0;Players Currently Online: " .. #player_names .. "]" ..
 		"button_exit[0,5.6;1.5,0.25;close;Close]" ..
 		"tableoptions[background=#314D4F]" ..
 		"tablecolumns[color;text,align=center,width=10]" ..
-		"table[0,0.6;2.8,4.5;Players Online;"
-
-	for _, player in ipairs(player_names) do
+		"table[0,0.6;2.8,4.5;player_list;"
+	local formspec_table = {}
+	for index, player in ipairs(player_names) do
 		if player == localplayer:get_name() then
 			-- use muted color for localplayer
-			formspec = formspec .. "#569784," .. player .. ","
+			formspec_table[index] = "#569784," .. player
 		else
-			formspec = formspec .. "#EEEEEE," .. player .. ","
+			formspec_table[index] = "#EEEEEE," .. player
 		end
 	end
+	formspec = formspec .. table.concat(formspec_table, ",") .. ";]"
+	
+	if selected_player ~= "" then
+		formspec = formspec .. "button[3.05,1;1.5,0.25;kick;Kick]"
+			.. "button[3.05,2;1.5,0.25;ban;Ban]"
 
-	formspec = formspec .. ";]"
-
-	minetest.show_formspec("who", formspec)
+	end
+	minetest.show_formspec("who_plus:player_list", formspec)
 
 end
 
 minetest.register_chatcommand("who", {
 	func = function(param)
-		show_players()
+		show_main_dialog()
 	end,
 })
 
-minetest.display_chat_message("[CSM] loaded Who")
+core.register_on_formspec_input(function(formname, fields)
+	if formname ~= "who_plus:player_list" then return false end
+	if fields.player_list then
+		local selected = fields.player_list
+		if selected:sub(1,3) == "CHG" then
+			local index = selected:sub(5,5)
+			selected_player = player_names[tonumber(index)]
+			show_main_dialog()
+		end
+		return true
+	end
+	if fields.quit then
+		-- Run cleanup code
+		selected_player = ""
+		return true
+	end
+	if fields.kick then
+		minetest.run_server_chatcommand("kick", selected_player)
+		show_main_dialog()
+		return true
+	end
+
+	if fields.ban then
+		minetest.run_server_chatcommand("ban", selected_player)
+		show_main_dialog()
+		return true
+	end
+end)
+
